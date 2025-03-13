3e59fad2008e5c5850857cd4c2038e0932d4d472
HamzaAftabKhan <hk5173920@gmail.com>
2025-03-13 17:37:15 +0500

    Class Task

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5120;
 
 // Define a simple route
 app.get('/', (req, res) => {
